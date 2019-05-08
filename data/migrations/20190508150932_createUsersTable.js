d61f486bbffb3f12bf81890f0d0d41b9bd424e08
sriley <sarahriley.dev@gmail.com>
2019-05-08 15:19:21 -0400

    added users table

---
+++ data/migrations/20190508150932_createUsersTable.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(tbl) {
+    tbl.increments();
+
+    tbl.string('name', 128).notNullable();
+
+    tbl
+    .integer('role_id')//..
+    .unsigned() //include this because some db ms need it
+    .references('id')//primary key in the parent table
+    .inTable('roles')//the name of the parent table
+    .onDelete('CASCADE') //delete joined child ids as well
+    .onUpdate('CASCADE')
+
+    tbl.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
